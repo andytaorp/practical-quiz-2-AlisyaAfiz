772173329a953bd5d511e3370e644d840f9b4f62
AlisyaAfiz <23018913@myrp.edu.sg>
2024-11-27 15:45:38 +0800

    Update

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,7 +12,7 @@ export default function Form({ onAddTask }) {
   return (
     /*TODO: add a form to add a new task*/
     <form onSubmit={handleSubmit}>
-      <input type="text" value={description}></input>
+      <input type="text" placeholder="Task..." value={description}></input>
       <button type="submit">Add</button>
     </form>
   );
