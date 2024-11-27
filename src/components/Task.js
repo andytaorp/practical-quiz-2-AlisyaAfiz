026eb4ce5e1d05672cb4500a13c6374a0572186b
AlisyaAfiz <23018913@myrp.edu.sg>
2024-11-27 15:54:14 +0800

    Update

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,11 +6,12 @@ import React from "react";
 //  * @returns {React.Component} A <li> containing the task description and a checkbox to toggle completion
 //  */
 
-export default function Task({task, handleToggleTask}) {
+export default function Task({task, handleToggleTask, handleDeleteTask}) {
     return (
         <li>
             <input type="checkbox" checked={task.status} onChange={handleToggleTask}></input>
             {task.description}
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
         </li>
     )
 }
